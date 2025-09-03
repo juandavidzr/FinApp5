66d683aa77aaa9bd99b12d889083d38bdce67786
manumendez1 <manuelamendezgonzalez12@gmail.com>
2025-09-03 16:28:44 -0500

    Interfaz nueva autenticación app móvil

--- FinApp5/MainPage.xaml.cs
+++ FinApp5/MainPage.xaml.cs
@@ -1,24 +1,16 @@
 ﻿using FinApp5.Modelo;
 using FinApp5.ViewModels;
-using FinApp5.Views;
-using System.Windows.Input;
 
 namespace FinApp5
 {
     public partial class MainPage : ContentPage
     {
-        Musuarios Usuario = new Musuarios();
         public MainPage()
         {
             InitializeComponent();
-            Loaded += (s, e) => SetFocus();
-            TxtUsuario.Text = string.Empty;
-            TxtPW.Text = string.Empty;
-            BindingContext = new VMingresar(Navigation, Usuario);
-        }
-        private void SetFocus()
-        {
-            TxtUsuario.Focus();
+
+            // Conectamos la vista con el ViewModel
+            BindingContext = new VMingresar(Navigation, new Musuarios());
         }
     }
 }
